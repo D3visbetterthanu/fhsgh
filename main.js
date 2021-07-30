d97803110ee11cd57705f40bf7db13a9f690e42c
D3visbetterthanu <79053589+D3visbetterthanu@users.noreply.github.com>
2021-07-30 12:09:51 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var canvass= new fabric.Canvas("canvass");
+var BD_epic=" ";
+function pogchampp (){
+    fabric.Image.fromURL("BirthdayImage.jpg",function(Img){
+
+        BD_epic=Img;
+        BD_epic.scaleToWidth(150);
+        BD_epic.scaleToHeight(140);
+        BD_epic.set({
+            top: 0,
+            left: 0,
+
+        });
+        canvas.add(BD_epic);
+    });
+}
+function playsound(){ 
+       x.play();
+}
